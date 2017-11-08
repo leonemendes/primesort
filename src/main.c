0d20c5ca967bb327b6a87b36065d6903a77ccccd
leonemendes <leonemendes23@gmail.com>
2017-11-08 00:04:40 -0200

    Working code

--- src/main.c
+++ src/main.c
@@ -7,11 +7,39 @@
 
 #include <stdio.h>
 
+int prime(int num){
+	
+	if(num == 1) return 0;
+	if(num == 2) return 1;
+	if(num % 2 == 0) return 0;
+	
+	for(int i = 3; i <= num/2; i+= 2){
+		
+		if ((num % i) == 0) return 0;
+	}
+	return 1;
+}
+
 int main() {
 
-  int x, y;
-
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+	int num_in;
+  FILE *sort;
+  
+  sort = popen("sort -n -r", "w");
+  
+  
+  while(scanf("%d\n", &num_in) != EOF){
+  	
+  	if(num_in == -1) break;
+  	
+  	else{
+  	
+  		if((prime(num_in) == 0)) fprintf(sort, "%d\n", num_in);
+  	}
+  	  
+  }
+  
+  pclose(sort);
+    
   return 0;
 }
